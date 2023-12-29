5fda93d6ee8e2728369decadb7a848b19c97bbce
Kristfur <>
2023-12-29 23:23:27 +0000

    Add dashboard ml evaluation page

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -2,26 +2,78 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_ml_performance_metrics():
-    version = 'v1'
 
-    st.write("### Train, Validation and Test Set: Labels Frequencies")
+    if st.checkbox("Version 1"):
+        version = 'v1'
 
-    label_distribution = plt.imread(f"outputs/{version}/label_distribution.png")
-    st.image(label_distribution, caption='Label Distribution on Train, Validation and Test Sets')
-    st.write("---")
+        st.write("### Train, Validation and Test Set: Labels Frequencies")
 
+        labels_distribution = plt.imread(
+            f"outputs/{version}/labels_distribution.png")
+        st.image(labels_distribution,
+                 caption="Labels Distribution on Train, Validation and Test Sets")
+        st.write("---")
 
-    version = 'v2'
-    st.write("### Model History")
-    col1, col2 = st.beta_columns(2)
-    with col1: 
-        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_acc, caption='Model Training Accuracy')
-    with col2:
-        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
-        st.image(model_loss, caption='Model Training Losses')
-    st.write("---")
-    
+        st.write("### Model History")
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+            st.image(model_acc, caption='Model Training Accuracy')
+        with col2:
+            model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+            st.image(model_loss, caption='Model Training Losses')
+        st.write("---")
+
+    if st.checkbox("Version 2"):
+        version = 'v2'
+
+        st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+        labels_distribution = plt.imread(
+            f"outputs/{version}/labels_distribution.png")
+        st.image(labels_distribution,
+                 caption="Labels Distribution on Train, Validation and Test Sets")
+        st.write("---")
+
+        st.write("### Model History")
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+            st.image(model_acc, caption='Model Training Accuracy')
+        with col2:
+            model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+            st.image(model_loss, caption='Model Training Losses')
+        st.write("---")
+
+        st.write("### Generalised Performance on Test Set")
+        st.dataframe(pd.DataFrame(load_test_evaluation(
+            version), index=['Loss', 'Accuracy']))
+
+    if st.checkbox("Version 3"):
+        version = 'v3'
+
+        st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+        labels_distribution = plt.imread(
+            f"outputs/{version}/labels_distribution.png")
+        st.image(labels_distribution,
+                 caption="Labels Distribution on Train, Validation and Test Sets")
+        st.write("---")
+
+        st.write("### Model History")
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+            st.image(model_acc, caption='Model Training Accuracy')
+        with col2:
+            model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+            st.image(model_loss, caption='Model Training Losses')
+        st.write("---")
+
+        st.write("### Generalised Performance on Test Set")
+        st.dataframe(pd.DataFrame(load_test_evaluation(
+            version), index=['Loss', 'Accuracy']))
